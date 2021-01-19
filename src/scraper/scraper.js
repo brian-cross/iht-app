9a998945b2c63a93c661ae38638ceed3f092a532
Brian Cross <briancross@shaw.ca>
2021-01-19 13:08:08 -0700

    Installed dotenv and implemented page login.

--- src/scraper/scraper.js
+++ src/scraper/scraper.js
@@ -1,14 +1,19 @@
 const puppeteer = require("puppeteer");
+require("dotenv").config();
 
 async function test() {
-  const serviceCodesUrl = "https://thepipe.sjrb.ca/docs/DOC-42236";
-  const options = { headless: false, slowMo: 250 };
+  console.log(process.env.SERVICE_CODES_URL);
+  const options = { headless: false, slowMo: 100 };
   const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
-  await page.goto(serviceCodesUrl);
+  await page.goto(process.env.SERVICE_CODES_URL);
   await page.waitForSelector("#sso-login-submit");
   await page.click("#sso-login-submit");
-  await browser.close;
+  await page.waitForSelector("#userNameInput");
+  await page.type("#userNameInput", process.env.LOGIN_EMAIL);
+  await page.type("#passwordInput", process.env.LOGIN_PASSWORD);
+  await page.click("#submitButton");
+  // await browser.close();
 }
 
 test();
