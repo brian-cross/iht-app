312f693108ae05e9cb8dc53e042e6237f7eb82b4
Brian Cross <briancross@shaw.ca>
2021-01-20 08:41:55 -0700

    Debugging puppeteer code.

--- src/scraper/scraper.js
+++ src/scraper/scraper.js
@@ -2,17 +2,29 @@ const puppeteer = require("puppeteer");
 require("dotenv").config();
 
 async function test() {
-  console.log(process.env.SERVICE_CODES_URL);
-  const options = { headless: false, slowMo: 100 };
+  const { LOGIN_EMAIL, LOGIN_PASSWORD, SERVICE_CODES_URL } = process.env;
+
+  const options = { headless: false };
   const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
-  await page.goto(process.env.SERVICE_CODES_URL);
-  await page.waitForSelector("#sso-login-submit");
+
+  await page.goto(SERVICE_CODES_URL);
+
+  await page.waitForSelector("#sso-login-submit", { visible: true });
   await page.click("#sso-login-submit");
+
   await page.waitForSelector("#userNameInput");
-  await page.type("#userNameInput", process.env.LOGIN_EMAIL);
-  await page.type("#passwordInput", process.env.LOGIN_PASSWORD);
+  await page.type("#userNameInput", LOGIN_EMAIL);
+  await page.type("#passwordInput", LOGIN_PASSWORD);
   await page.click("#submitButton");
+
+  const tables = await page.evaluate(() => {
+    // return document.querySelectorAll("table.tablesorter");
+    return 1;
+  });
+
+  console.log(tables);
+
   // await browser.close();
 }
 
