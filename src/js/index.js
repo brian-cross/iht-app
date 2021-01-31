def6f14462b368c59dbef83051018be7279e27f0
Brian Cross <briancross@shaw.ca>
2021-01-30 19:32:20 -0700

    index.js now only contains code global to the whole site.

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,42 @@
-import { createDiv } from "./dom";
-import getHolidays from "./holidays";
-import months from "./months";
-import monthContainer from "./calendar";
-import "./scroll";
+/*******************************************************************************
+ * Hides / shows header on scroll
+ ******************************************************************************/
+const header = document.querySelector("header");
 
-if (!localStorage.getItem("week")) {
-  localStorage.setItem("week", "1");
-}
+let currentY = window.pageYOffset,
+  scrolledY = window.pageYOffset,
+  delta = 0;
 
-let currentWeek = localStorage.getItem("week");
-let currentYear = new Date().getFullYear();
+window.addEventListener("scroll", () => {
+  scrolledY = window.pageYOffset;
+  delta = scrolledY - currentY;
+  // Hide header when page is scrolled down
+  if (delta > 5) {
+    header.classList.add("hidden");
+    currentY = scrolledY;
+  }
+  // Show header when page is scrolled up or when scroll is close to top
+  if (delta < -5 || scrolledY < 75) {
+    header.classList.remove("hidden");
+    currentY = scrolledY;
+  }
+});
+
+/*******************************************************************************
+ * Handles opening / closing of the menu
+ ******************************************************************************/
 let menuIsOpen = false;
 
-// Mobile menu button handler
 const hamburger = document.querySelector(".hamburger");
 const mainMenu = document.querySelector(".main-menu");
 hamburger.addEventListener("click", handleMenuOpenClose);
-
-// Nav link handlers
-const navLinks = document.querySelectorAll(".main-menu-item");
-navLinks.forEach(navLink => {
-  navLink.addEventListener("click", e => {
-    currentWeek = e.currentTarget.dataset.week;
-    localStorage.setItem("week", currentWeek);
-    renderCalendar(currentYear, currentWeek);
-    handleMenuOpenClose();
-  });
-});
 
 // Page overlay for closing the menu
 const overlay = document.querySelector(".overlay");
 overlay.addEventListener("click", handleMenuOpenClose);
 
 // Open or close the menu
-function handleMenuOpenClose() {
+export function handleMenuOpenClose() {
   menuIsOpen = !menuIsOpen;
   if (menuIsOpen) {
     hamburger.classList.add("open");
@@ -45,19 +48,3 @@ function handleMenuOpenClose() {
     overlay.style.display = "none";
   }
 }
-
-// Create a container to hold the calendar
-const calendarContainer = createDiv("", "calendar-container");
-document.querySelector(".page-container").appendChild(calendarContainer);
-
-// Render the calendar after removing any existing calendar
-async function renderCalendar(year, week) {
-  const holidays = await getHolidays(year); // Get list of stat holidays for the year
-  while (calendarContainer.firstChild)
-    calendarContainer.removeChild(calendarContainer.firstChild);
-  months.forEach(month =>
-    calendarContainer.appendChild(monthContainer(year, month, week, holidays))
-  );
-}
-
-renderCalendar(currentYear, currentWeek);
