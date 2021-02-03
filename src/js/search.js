691d3be15943d0d71d56a8a68591d3a4b2a9f13c
Brian Cross <briancross@shaw.ca>
2021-02-02 18:16:44 -0700

    Render search results to the DOM.

--- src/js/search.js
+++ src/js/search.js
@@ -13,12 +13,12 @@ const resultList = document.querySelector(".search-result-list");
 
 searchInput.addEventListener("input", handleSearchInput);
 
-let searchResults;
+let searchResults = [];
 let inputDebounceId;
-const inputDebounceInverval = 300;
+const inputDebounceInverval = 500;
 
-// Search input event handler.
-// Runs the search after a debounce interval.
+// Search input event handler
+// Runs the search after a debounce interval
 function handleSearchInput(e) {
   clearTimeout(inputDebounceId);
   inputDebounceId = setTimeout(
@@ -28,24 +28,53 @@ function handleSearchInput(e) {
 }
 
 function doCodeSearch(searchString) {
-  if (searchString.length === 0) return;
+  if (searchString.length === 0) searchResults = [];
+  else {
+    const regex = new RegExp(searchString, "i");
 
-  const regex = new RegExp(searchString, "i");
+    const serviceCodeResults = serviceCodes.filter(code => {
+      if (
+        regex.test(code.code) ||
+        regex.test(code.title) ||
+        regex.test(code.description)
+      )
+        return code;
+    });
 
-  const serviceCodeResults = serviceCodes.filter(code => {
-    if (
-      regex.test(code.code) ||
-      regex.test(code.title) ||
-      regex.test(code.description)
-    )
-      return code;
-  });
+    const tbcCodeResults = tbcCodes.filter(code => {
+      if (regex.test(code.code) || regex.test(code.title)) return code;
+    });
 
-  const tbcCodeResults = tbcCodes.filter(code => {
-    if (regex.test(code.code) || regex.test(code.title)) return code;
-  });
+    searchResults = [...tbcCodeResults, ...serviceCodeResults];
+  }
 
-  searchResults = [...tbcCodeResults, ...serviceCodeResults];
+  renderSearchResults(searchResults);
+}
 
-  console.log(searchResults);
+function renderSearchResults(searchResults) {
+  while (resultList.firstChild) resultList.removeChild(resultList.firstChild);
+
+  searchResults
+    .map(result => {
+      const listItem = document.createElement("li");
+
+      listItem.classList.add("search-result-item");
+      listItem.classList.add(result.rate ? "tbc-code" : "service-code");
+
+      listItem.innerHTML = `
+      <div class="search-result-item-header">
+        <h2 class="code-number">${result.code}</h2>
+        <h3 class="code-rate">${result.rate ? result.rate : ""}</h3>
+      </div>
+      <h3 class="code-title">${result.title}</h3>
+      <p class="code-description">${
+        result.description ? result.description : ""
+      }</p>
+    `;
+
+      return listItem;
+    })
+    .forEach(result => resultList.appendChild(result));
+
+  // console.log(listItems);
 }
